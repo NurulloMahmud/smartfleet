61b8c40192433a4279d6149abcad79c6d8e53324
Nurullo <94496332+NurullohMahmud@users.noreply.github.com>
2024-01-06 09:57:53 -0600

    project set up

---
+++ smartfleet/urls.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path(os.getenv('ADMIN_URL'), admin.site.urls),
+    path('users/', include('users.urls')),
+]
