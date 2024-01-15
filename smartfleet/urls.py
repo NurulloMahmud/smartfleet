503faa81059cf459eab15dd1c4acc7f98193e6ce
Nurullo <94496332+NurullohMahmud@users.noreply.github.com>
2024-01-15 16:25:40 -0600

    swagger

--- smartfleet/urls.py
+++ smartfleet/urls.py
@@ -1,11 +1,30 @@
 import os
 from dotenv import load_dotenv
 
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 load_dotenv()
 
 
 from django.contrib import admin
 from django.urls import path, include
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 
 urlpatterns = [
@@ -15,4 +34,9 @@ urlpatterns = [
     path('fleet/', include('fleet.urls')),
     path('hiring/', include('hiring.urls')),
     path('maintenance/', include('maintenance.urls')),
+    
+    # swagger
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
