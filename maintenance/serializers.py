c8b80c4cc53b401f8e43aa7e2073f097cf18e87a
nurulloh <nurullomahmud@gmail.com>
2024-01-16 03:04:24 +0500

    case crud

--- maintenance/serializers.py
+++ maintenance/serializers.py
@@ -1,5 +1,9 @@
 from rest_framework import serializers
-from maintenance.models import Status
+from maintenance.models import Status, Case
+
+from fleet.models import Truck
+from fleet.serializers import TruckListSerializer
+
 
 
 
@@ -7,4 +11,27 @@ class StatusSerializer(serializers.ModelSerializer):
     class Meta:
         model = Status
         fields = ['name']
+
+
+class CaseWriteSerializer(serializers.ModelSerializer):
+    truck = serializers.PrimaryKeyRelatedField(
+        queryset = Truck.objects.all()
+    )
+
+    status = serializers.ModelSerializer(
+        queryset = Status.objects.all()
+    )
+
+    class Meta:
+        model = Case
+        fields = '__all__'
+
+
+class CaseReadSerializer(serializers.ModelSerializer):
+    truck = TruckListSerializer()
+    status = StatusSerializer()
+
+    class Meta:
+        model = Case
+        fields = '__all__'
 
--- maintenance/views.py
+++ maintenance/views.py
@@ -1,7 +1,11 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
 
-from maintenance.models import Status
-from maintenance.serializers import StatusSerializer
+from maintenance.models import Status, Case
+from maintenance.serializers import CaseWriteSerializer, StatusSerializer, CaseReadSerializer
 
 
 
@@ -9,3 +13,28 @@ class StatusViewset(ModelViewSet):
     queryset = Status.objects.all()
     serializer_class = StatusSerializer
 
+
+class CaseListCreateView(APIView):
+    def get(self, request):
+        queryset = Case.objects.all()
+        serializer = CaseReadSerializer(queryset, many=True)
+
+        return Response({"Success": True , "data": serializer.data}, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        data = request.data
+        serializer = CaseWriteSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response({"Success": True, "data": serializer.data}, status=status.HTTP_201_CREATED)
+        
+        return Response({"Success": False}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CaseRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Case.objects.all()
+    serializer_class = CaseWriteSerializer
+
+ 
