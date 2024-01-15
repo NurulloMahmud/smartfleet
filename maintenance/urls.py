7216aa69d79288908ab69c82cf4a34dfec15ed02
nurulloh <nurullomahmud@gmail.com>
2024-01-15 16:33:04 +0500

    maintenance case status

--- maintenance/urls.py
+++ maintenance/urls.py
@@ -1,8 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+
+from maintenance.views import StatusViewset
+
+router = DefaultRouter()
+router.register(r'status', StatusViewset, basename='status')
+
 
 
 app_name = "maintenance"
 
 urlpatterns = [
-    
+    path('', include(router.urls))
 ]
--- maintenance/views.py
+++ maintenance/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from maintenance.models import Status
+from maintenance.serializers import StatusSerializer
+
+
+
+class StatusViewset(ModelViewSet):
+    queryset = Status.objects.all()
+    serializer_class = StatusSerializer
+
