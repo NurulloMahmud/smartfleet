0a768d7d7dd190871d030267bf213833c254c1e3
nurulloh <nurullomahmud@gmail.com>
2024-01-17 17:47:44 +0500

    permissions

--- hiring/permissions.py
+++ hiring/permissions.py
@@ -1,10 +1,10 @@
 from rest_framework import permissions
 
 
-class ManagerPermission(permissions.BasePermission):
+class HiringPermission(permissions.BasePermission):
     def has_permission(self, request, view):
 
         if request.method in permissions.SAFE_METHODS:
             return True
         
-        return request.user.department.name.lower() == 'management'
+        return request.user.department.name.lower() in ('hiring', 'management')
