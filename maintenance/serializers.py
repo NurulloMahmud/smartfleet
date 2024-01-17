1b924d862c805a17e197f47b33a2617ce0f4a133
Nurullo <94496332+NurullohMahmud@users.noreply.github.com>
2024-01-17 14:00:56 -0600

    custom validation for odomter serializer

--- maintenance/serializers.py
+++ maintenance/serializers.py
@@ -1,3 +1,5 @@
+from django.shortcuts import get_object_or_404
+
 from rest_framework import serializers
 from maintenance.models import Status, Case, Note, Odometer
 
@@ -59,3 +61,43 @@ class OdometerWriteSerializer(serializers.ModelSerializer):
         model = Odometer
         fields = '__all__'
 
+    def validate(self, data):
+        """
+        validate incoming value for ododmeter
+        1 - incoming value cannot be less than 0
+        2 - incoming value for odometer cannot be less than latest odometer value
+        3 - do not record the odometer value if it is more than 10k compared to last week's
+        """
+        truck_id = data.get('truck')
+        current_odometer = data.get('odometer')
+
+        # validate truck instance
+        try:
+            truck_instance = Truck.objects.get(id=truck_id)
+        except Truck.DoesNotExist:
+            raise serializers.ValidationError("Truck does not exist")
+
+        # validate current odometer
+        if current_odometer < 0:
+            raise serializers.ValidationError("odometer cannot be in negative")
+
+        # if this is the first odometer record for truck instance, complete the validation
+        if not Odometer.objects.filter(truck=truck_instance).exists():
+            return data
+
+        # Get the last odometer instance, if it exists
+        last_odometer_instance = Odometer.objects.filter(truck=truck_instance).order_by('-date').first()
+        
+        if last_odometer_instance:
+            last_odometer = last_odometer_instance.odometer
+            last_week = timezone.now().date() - timedelta(days=7)
+
+            # Validate the 10k mileage difference within the last week
+            if last_odometer_instance.date >= last_week and last_odometer + 10000 < current_odometer:
+                raise serializers.ValidationError("Mileage difference is suspicious.")
+
+            # Odometer reading cannot decrease over time
+            if current_odometer < last_odometer:
+                raise serializers.ValidationError("Odometer reading cannot decrease from the previous reading.")
+
+        return data
