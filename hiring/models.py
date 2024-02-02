51d013fd277e18408fa942d264fde0923af206f6
NurullohMahmud <nurullomahmud@gmail.com>
2024-02-02 13:49:34 -0600

    hiring status & soft delete

--- hiring/models.py
+++ hiring/models.py
@@ -3,6 +3,18 @@ from management.models import Company
 
 
 
+class Status(models.Model):
+    name = models.CharField(max_length=50)
+    deleted = models.BooleanField(default=False)
+    last_update = models.DateField(auto_now=True)
+
+    def delete(self, *args, **kwargs):
+        self.deleted = True
+        self.save()
+    
+    def __str__(self) -> str:
+        return self.name
+
 
 class Driver(models.Model):
     first_name = models.CharField(max_length=50)
@@ -14,6 +26,13 @@ class Driver(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE, null=True, blank=True)
     truck = models.ForeignKey('fleet.Truck', on_delete=models.CASCADE, 
                               null=True, blank=True, related_name='assigned_driver')
+    status = models.ForeignKey(Status, on_delete=models.CASCADE, null=True, blank=True)
+    deleted = models.BooleanField(default=False)
+    last_update = models.DateField(auto_now=True)
+
+    def delete(self, *args, **kwargs):
+        self.deleted = True
+        self.save()
     
     def __str__(self):
         return self.first_name
